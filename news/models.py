e994d4c01aea31be76f4de0469360cb038a6bf7c
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-13 14:15:57 +0300

    add authentication

--- news/models.py
+++ news/models.py
@@ -1,22 +1,23 @@
 from decouple import RepositoryEnv, RepositoryIni
 from django.db import models
 import datetime as dt
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Editor(models.Model):
-    first_name = models.CharField(max_length =30)
-    last_name = models.CharField(max_length =30)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length = 10,blank =True)
+# class Editor(models.Model):
+#     first_name = models.CharField(max_length =30)
+#     last_name = models.CharField(max_length =30)
+#     email = models.EmailField()
+#     phone_number = models.CharField(max_length = 10,blank =True)
     
-    def __str__(self):
-        return self.first_name
+#     def __str__(self):
+#         return self.first_name
     
-    class Meta:
-        ordering = ['first_name']
+#     class Meta:
+#         ordering = ['first_name']
         
-    def save_editor(self):
-        self.save()
+#     def save_editor(self):
+#         self.save()
         
 # database r/ships
 class tags(models.Model):
@@ -24,14 +25,14 @@ class tags(models.Model):
 
     def __str__(self):
         return self.name
-    
+from tinymce.models import HTMLField   
 class Article(models.Model):
     title = models.CharField(max_length =60)
-    post = models.TextField()
-    editor = models.ForeignKey(Editor,on_delete=models.CASCADE)
+    post = HTMLField()
+    editor = models.ForeignKey(User,on_delete=models.CASCADE)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
-    article_image = models.ImageField(upload_to = 'articles/')
+    article_image = models.ImageField(upload_to = 'articles/', blank=True)
     
     def __str__(self):
         return self.title
